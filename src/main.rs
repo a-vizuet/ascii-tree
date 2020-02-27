e87660f19150e26f54fdf5a1cb1a7e96b2a43455
megustalafantabienfria <megustalafantabienfria@hotmail.com>
2020-02-27 17:05:02 -0600

    Now it outputs the tree recursively

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,13 @@ enum TypeElement {
 struct Element {
     type_el: TypeElement,
     name: OsString,
-    childs: Option<Vec<Element>>,
+    children: Option<Vec<Element>>,
 }
 
 fn main() {
     match read_folder(".") {
-        Ok(a) => println!("{0}", to_ascii(a)),
-        Err(e) => println!("{:?}", e),
+        Ok(readed_folder) => println!("{0}", init_ascii_build(readed_folder)),
+        Err(error) => println!("{:?}", error),
     }
 }
 
@@ -30,18 +30,24 @@ fn read_folder(path: &str) -> Result<Vec<Element>, Error> {
             elements.push(Element {
                 type_el: TypeElement::FILE,
                 name: entry.file_name(),
-                childs: None,
+                children: None,
             });
         } else {
             elements.push(Element {
                 type_el: TypeElement::DIRECTORY,
                 name: entry.file_name(),
-                childs: Option::from(read_folder(entry.path().to_str().unwrap())?),
+                children: Some(read_folder(entry.path().to_str().unwrap())?),
             });
         }
     }
 
     Ok(elements)
+}
+
+fn init_ascii_build(elements: Vec<Element>) -> String {
+    let mut ascii_tree = String::new();
+    ascii_tree.push_str("Main folder\n");
+    to_ascii(Some(ascii_tree), elements, 0)
 }
 
 /**
@@ -50,20 +56,18 @@ fn read_folder(path: &str) -> Result<Vec<Element>, Error> {
  * |--> Secondary folder
  * |    |-> File
  */
-fn to_ascii(elements: Vec<Element>) -> String {
-    let mut ascii_tree = String::new() + "Main folder\n";
-
+fn to_ascii(ascii_tree: Option<String>, elements: Vec<Element>, level: i32) -> String {
+    let mut unwrapped_ascii_tree: String = ascii_tree.unwrap_or("".to_string());
     for element in elements {
-        ascii_tree += "|";
-        match element.type_el {
-            TypeElement::FILE => {
-                ascii_tree += &format!("--> {:?} \n", element.name.to_str().unwrap()).to_string();
-            }
-            TypeElement::DIRECTORY => {
-                ascii_tree += &format!("--> {:?} \n", element.name.to_str().unwrap()).to_string()
-            }
+        unwrapped_ascii_tree.push_str(&"|   ".repeat(level as usize));
+
+        unwrapped_ascii_tree
+            .push_str(&format!("--> {:?} \n", element.name.to_str().unwrap()).to_string());
+
+        if format!("{:?}", element.type_el) == format!("{:?}", TypeElement::DIRECTORY) {
+            unwrapped_ascii_tree.push_str(&to_ascii(None, element.children.unwrap(), level + 1));
         }
     }
 
-    ascii_tree.to_string()
+    unwrapped_ascii_tree.to_string()
 }
